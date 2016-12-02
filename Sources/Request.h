15373ead2f6c3283d92936b13dfaff65185f4d97
Roman Roibu <romanroibu@gmail.com>
2016-12-02 18:26:29 +0100

    Add empty Xcode project

---
+++ Sources/Request.h
@@ -0,0 +1,19 @@
+//
+//  Request.h
+//  Request
+//
+//  Created by Roman Roibu on 02/12/2016.
+//  Copyright © 2016 Roman Roibu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Request.
+FOUNDATION_EXPORT double RequestVersionNumber;
+
+//! Project version string for Request.
+FOUNDATION_EXPORT const unsigned char RequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Request/PublicHeader.h>
+
+
